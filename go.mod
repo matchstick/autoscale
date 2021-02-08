075c3e0486852db641e097a889384b921400583d
Michael Rubin <mhr@neverthere.org>
2021-02-07 23:38:29 -0800

    Using flags pkg for now. Retrieved deployment.
    Got the flags we wanted for quick use just two of deployment nad replicas.
    
    Retrieve the deployment now.

--- go.mod
+++ go.mod
@@ -4,8 +4,20 @@ go 1.15
 
 require (
 	github.com/golangci/golangci-lint v1.36.0
+	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/viper v1.7.1
 	golang.org/x/tools v0.1.0 // indirect
+	// As per https://github.com/kubernetes/client-go/issues/551
+	// we need to run:
+	// go mod init
+	// go mod vendor
+	// go mod downlaod
+	// Then pin the version of:
+	// k8s.io/api, k8s.io/apimachinery and k8s.io/client-go
+	k8s.io/api v0.20.2
+	k8s.io/apimachinery v0.20.2
+	k8s.io/client-go v0.20.2
+	k8s.io/utils v0.0.0-20210111153108-fddb29f9d009 // indirect
 )
--- main.go
+++ main.go
@@ -17,13 +17,78 @@ limitations under the License.
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	//	appsv1 "k8s.io/api/apps/v1"
+
+	apiv1 "k8s.io/api/core/v1"
+
+	"strconv"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	//	"k8s.io/client-go/util/retry"
+
 	// Adding the golangci-lint import here as a way to insure
 	// that it is installed automatically with a standard version
 	// when we either build the product or run tests.
 	_ "github.com/golangci/golangci-lint/pkg/commands"
-	"github.com/matchstick/autoscale/cmd"
 )
 
+const numArgs = 2
+
 func main() {
-	cmd.Execute()
+	var kubeconfig *string
+
+	home := homedir.HomeDir()
+	if home != "" {
+		kubeconfig =
+			flag.String("kubeconfig",
+				filepath.Join(home, ".kube", "config"),
+				"(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	flag.Parse()
+
+	if len(flag.Args()) != numArgs {
+		fmt.Println("Please specify <eployment> <replicas>")
+		os.Exit(1)
+	}
+
+	target := flag.Arg(0)
+	replicaStr := flag.Arg(1)
+
+	replicas, err := strconv.Atoi(replicaStr)
+	if err != nil {
+		fmt.Println("Replicas must be an integer")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Setting Deplyment %s to %d replicas.", target, replicas)
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// get and update replicas
+	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	deployment, _ := deploymentsClient.Get(context.TODO(), target, metav1.GetOptions{})
+	// TODO
+	//	deploymentsClient.Update(context.TODO(), deployment, metav1.UpdateOptions{})
+	fmt.Printf("%#v", deployment)
 }
